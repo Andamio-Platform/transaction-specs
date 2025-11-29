6f77207db5b9df16367747e2deb907996a52e122
nelsonksh <nelsonksh@gimbalabs.io>
2025-11-29 12:38:29 +0530

    wip: classifier

--- classifier/go.mod
+++ classifier/go.mod
@@ -4,12 +4,17 @@ go 1.25.2
 
 require (
 	connectrpc.com/connect v1.19.1
+	github.com/Salvionied/apollo v1.4.0
 	github.com/utxorpc/go-codegen v0.18.1
 	github.com/utxorpc/go-sdk v0.0.0-20251126015131-5fdef4efddfd
 )
 
 require (
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 )
--- classifier/teacher_course_modules_manage.go
+++ classifier/teacher_course_modules_manage.go
@@ -1,20 +1,40 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/hex"
+	"fmt"
 
+	"github.com/Salvionied/apollo/serialization/PlutusData"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/cardano"
 )
 
 func TeacherCourseModulesManage(tx *cardano.Tx) bool {
-
-	// Resolve Hash of Reference Inputs Scripts
-	// Check if `0881d005d4301748df5aab08fbd302ad62f06a1b6b154664c96b9ba7` is the hash
+	targetHash := "0881d005d4301748df5aab08fbd302ad62f06a1b6b154664c96b9ba7"
 
 	referenceInputs := tx.GetReferenceInputs()
-	referenceInputsJSON, _ := json.MarshalIndent(referenceInputs, "", "  ")
-	println(string(referenceInputsJSON))
+	for _, refInput := range referenceInputs {
+		out := refInput.GetAsOutput()
+		if out == nil {
+			continue
+		}
+
+		script := out.GetScript()
+		if script == nil || script.GetPlutusV3() == nil {
+			continue
+		}
+
+		plutusV3 := PlutusData.PlutusV3Script(script.GetPlutusV3())
+
+		hash, err := plutusV3.Hash()
+		if err != nil {
+			fmt.Println("Error computing hash:", err)
+			continue
+		}
+
+		if hex.EncodeToString(hash.Bytes()) == targetHash {
+			return true
+		}
+	}
 
 	return false
-
 }
